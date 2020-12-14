ff5842c9675f87b5bf980ebe11434c9b282c40cd
Tracy Farah <tracy.farah@gmail.com>
2020-12-14 03:38:01 +0200

    .

--- quiz.js
+++ quiz.js
@@ -1,16 +1,41 @@
 let i = 0;
 let score = 0;
+let cat_number = localStorage["category"];
+let selected_category = localStorage["select_cat"];
+let beforesend=false;
 $(document).ready(function f() {
     $.ajax({
-        url: "https://opentdb.com/api.php?amount=10",
+        url: `https://opentdb.com/api.php?amount=10&category=${cat_number}`,
         dataType: "json",
+        beforeSend: function(){
+            if(beforesend==false){
+            $("#loader").show();
+            $("#frame").hide();
+            beforesend=true
+            }
+           },
+           complete:function(data){
+            $("#loader").hide();
+            $("#frame").show();
+           },
         success: function (obj) {
+            $('#quest_number span').html(i + 1);
+            $("#category").html(selected_category);
             $("#choices").html("");
             $("#bt_submit").html("Check Answer");
             const children = obj.results[i];
-            $("#question").html(children.question);
             let answers = [];
             console.log(children);//remove later
+            if (children.difficulty == 'easy') {
+                $('#diff span').css("color", "limegreen");
+            } else if (children.difficulty == 'medium') {
+                $('#diff span').css("color", "orange");
+            }
+            else if (children.difficulty == 'hard') {
+                $('#diff span').css("color", "red");
+            }
+            $('#diff span').html(children.difficulty);
+            $("#question").html(children.question);
             answers.push(children.correct_answer);
             children.incorrect_answers.forEach((val) => {
                 answers.push(val);
@@ -31,13 +56,31 @@ $(document).ready(function f() {
             $(".bt_choice").on("click", function () {
                 $(".bt_choice").removeAttr("id", "selected");
                 $(this).attr("id", "selected");
-
-
                 $("#bt_submit").on("click", function () {
                     if (clicked) {
                         $(this).off("click");
                         i++;
-                        f();
+                        if (i > 9) {
+                            $("#frame").html("");
+                            $("#frame").append("Total Score: " + score); //display score in a better way (html+css)
+                            let bt_playagain = document.createElement("button");
+                            bt_playagain.innerHTML = "Play Again";
+                            let bt_home = document.createElement("button");
+                            bt_home.innerHTML = "Return to Main Page";
+                            $("#frame").append('<br><br>');
+                            $("#frame").append(bt_playagain);
+                            $("#frame").append('<br><br>');
+                            $("#frame").append(bt_home);
+                            $(bt_playagain).on("click", () => {
+                                i = 0;
+                                window.location.href = "./category.html"
+                            })
+                            $(bt_home).on("click", () => {
+                                window.location.href = "./index.html"
+                            })
+                        } else {
+                            f();
+                        }
                     }
                     else {
                         clicked = true;
@@ -47,7 +90,14 @@ $(document).ready(function f() {
                         let answer = $("#selected").html();
                         if (answer == children.correct_answer) {
                             console.log("correct");
-                            score++;
+                            if (children.difficulty == 'easy') {
+                                score = score + 20;
+                            } else if (children.difficulty == 'medium') {
+                                score = score + 50;
+                            }
+                            else if (children.difficulty == 'hard') {
+                                score = score + 100;
+                            }
                             console.log(score);
                             $("#selected").css("background-color", "#29bb66");
                         }
